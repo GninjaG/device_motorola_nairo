c3592124edfafe7cb63d2ac8755178492b8adcd9
LMO GerritBot <gerrit@libremobileos.com>
2023-04-21 22:29:27 +0000

    Adapt to LMODroid by device-add script
    Change-Id: Icfb0b7365de073a215515de9a1c3fa1a7b6cccc0

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nairo.mk
+    $(LOCAL_DIR)/lmodroid_nairo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nairo-eng \
-    lineage_nairo-user \
-    lineage_nairo-userdebug
+    lmodroid_nairo-eng \
+    lmodroid_nairo-user \
+    lmodroid_nairo-userdebug
